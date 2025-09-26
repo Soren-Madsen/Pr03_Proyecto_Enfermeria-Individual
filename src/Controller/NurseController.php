10a1bdc343e43bf99a0910e651a2e66937bbd4aa
Soren-Madsen <sorenmadsen13@gmail.com>
2025-09-26 15:59:26 +0200

    Modified to nursecontroller

--- src/Controller/NurseController.php
+++ src/Controller/NurseController.php
@@ -6,10 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ListadoEnfermerosController extends AbstractController
+final class NurseController extends AbstractController
 {
-    #[Route('/listado/enfermeros', name: 'app_listado_enfermeros')]
-    public function index(): JsonResponse
+    #[Route('/nurse', name: 'app_listado_enfermeros')]
+    public function findByName(): JsonResponse
     {
         return $this->json([
             'message' => 'Welcome to your new controller!',
